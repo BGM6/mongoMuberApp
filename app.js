f5d0c8df6e4f14bbf576f19865fd76279b40bf69
bguilas <bryanjguilas@gmail.com>
2021-04-09 21:56:21 -0700

    added mongo connection

--- app.js
+++ app.js
@@ -1,6 +1,20 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const routes = require('./routes/routes');
 const app = express();
+
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost:27017/muber', {
+		useNewUrlParser: true,
+		useCreateIndex: true,
+		useUnifiedTopology: true,
+		useFindAndModify: false
+});
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'CONNECTION OPEN'));
+db.once('open', () => {
+		console.log('CONNECTION OPEN');
+});
 
 //middleware
 app.use(express.urlencoded({extended: true}));
--- test/controllers/driversControllers.js
+++ test/controllers/driversControllers.js
@@ -4,7 +4,6 @@ const request = require('supertest');
 const app = require('../../app');
 const Driver = mongoose.model('driver');
 
-
 describe('Drivers controller', () => {
 		it('Post to /api/drivers creates a new driver', (done) => {
 				Driver.count().then(count => {
